0b0776853e7732264e763dfe4a69c50664cc0949
Vineet Joshi <vineet0713@gmail.com>
2019-03-01 19:27:51 -0800

    Fix bug in EventGeneratorThread.

--- src/threads/StartGossipThread.java
+++ src/threads/StartGossipThread.java
@@ -4,20 +4,22 @@ package src.threads;
  * Thread for starting up the Gossip round.
  */
 public class StartGossipThread extends Thread {
+    private static final int GOSSIP_TIMEOUT = 60000;
 
     public void run() {
         System.out.println("StartGossipThread started.");
         while (true) {
             try {
-                //TODO - Implement this
-                Thread.sleep(10000);
-
-                //StartGossipRound()
-
+                Thread.sleep(GOSSIP_TIMEOUT);
+                startGossipRound();
             } catch (Exception e) {
                 System.out.println("Error in StartGossipThread caught. \n");
                 System.out.println("Something went wrong with the thread sleeping." + e.getStackTrace());
             }
         }
+    }
+
+    private void startGossipRound() {
+        // start gossip round
     }
 }
